ac82914553eaf1038b331862fffa9a38ed9be50b
Duarte3-ctrl <bandvdk3@gmail.com>
2026-02-13 16:00:05 -0800

    sesion 1 porfin

--- sesion_01.py
+++ sesion_01.py
@@ -1,1 +1,48 @@
-print("funciona por favor ")
+# NUMEROS Y FLOTANTES
+print(int(7))
+print(float(7.7))
+print(type(7))
+print(type(7.77))
+print(int(1+2))
+print(int(10*2))
+print("==operadores matematicos==")
+# +
+# -
+# *
+# /
+# ""
+# % modulos
+print(int(2**3))
+print(int(4**8))
+print(float(10%2))
+print(float(25%4))
+
+
+ventas = 1999999999
+print("nestra ventas fueron",ventas)
+
+
+is_active = True
+print(bool(is_active))
+
+
+game_over = False
+print(game_over)
+
+
+
+
+edad = 16
+if (edad >=18):
+    print("si puedo entrar a el bar¡")
+else:
+    print("no puedo entrar al bar")
+
+
+mi_numero = int(input("¿cual es el numero que deseas verficar?:"))
+print(f"el numero {mi_numero}")
+if mi_numero % 2 == 0:
+    print(f"el numero {mi_numero} es par¨¡¡¡")
+else:
+    print(f"el numero {mi_numero} es impar¡¡")
+
